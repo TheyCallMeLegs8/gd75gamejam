4fc86ceb67fd896f2d73ac262913d8c8cdb878ba
Alexandre Simao <GD75Alexandre@VFS.com>
2025-04-21 21:36:18 -0700

    added technical functionality for when player dies

---
+++ Assets/Scripts/PlayerDeathTrigger.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerDeathTrigger : MonoBehaviour
+{
+    [SerializeField] private LayerMask _hitLayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((_hitLayer & 1 << other.gameObject.layer) == 1 << other.gameObject.layer)
+        {
+            Debug.Log("Death");
+        }
+    }
+}
