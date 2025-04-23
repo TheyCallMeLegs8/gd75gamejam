0b08eb8f3b129dc3ba7f89cf6b9dacee15fc11d4
AlexandreSimao <ajsimao81@gmail.com>
2025-04-23 14:33:14 -0700

    Fixed FMod slider compiler errors

--- Assets/Scripts/FMOD/FMOD Slider.cs
+++ Assets/Scripts/FMOD/FMOD Slider.cs
@@ -4,24 +4,24 @@ using FMOD.Studio;
 
 public class VolumeControl : MonoBehaviour
 {
-    public Slider volumeSlider; // UI slider in Unity
+    //public Slider volumeSlider; // UI slider in Unity
     public FMOD.Studio.Bus musicBus;  // FMOD Bus (could be a VCA or a group)
 
     void Start()
     {
         // Get the FMOD Studio project's root sound bank
-        var project = FMODManager.GetRootGroup();
+        //var project = FMODManager.GetRootGroup();
 
         // Find the FMOD Bus (VCA or group)
-        musicBus = project.getBusByName("YourMusicBusName");
+        //musicBus = project.getBusByName("YourMusicBusName");
 
         // Initialize the slider's value (optional)
-        volumeSlider.value = musicBus.getVolume();
+        //volumeSlider.value = musicBus.getVolume();
     }
 
     void Update()
     {
         // Update the VCA's volume based on the slider's value
-        musicBus.setVolume(volumeSlider.value);
+        //musicBus.setVolume(volumeSlider.value);
     }
 }
